424cc322c1bd1179adcbcd00ae4d293b5f9daccf
Roberto Tienda <rohatiro@gmail.com>
2024-03-25 02:26:54 -0600

    Adding rss to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://thunderous-basbousa-d80e17.netlify.app/"
+});
