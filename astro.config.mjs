2d40776547ea2a74f39c45efb0124e6779b57e5f
Roberto Tienda <rohatiro@gmail.com>
2024-03-25 10:15:32 -0600

    building first astro island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://thunderous-basbousa-d80e17.netlify.app/"
+  site: "https://thunderous-basbousa-d80e17.netlify.app/",
+  integrations: [preact()]
 });
